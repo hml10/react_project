40dc2ef8d13319ec8d58e7b03f12b862b2f76d53
hml <2665076656@qq.com>
2020-03-28 23:19:36 +0800

    路由+登录功能

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,12 +4,23 @@ import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import "./css/login.less";
 import logo from "./imgs/logo.png";
 
-const { Item } = Form;
+const { Item } = Form; //架构From Item
 
 export default class Login extends Component {
   // 表单提交的回调
   onFinish = values => {
     console.log("接收到的表单数据: ", values);
+  };
+  // 密码的回调函数
+  pwdValidator = (_, value) => {
+    // console.log(value)
+    const errmsg = [];
+    if (!value.trim()) errmsg.push("密码必须输入");
+    if (value.length < 4) errmsg.push("密码必须大于等于4位");
+    if (value.length > 12) errmsg.push("密码必须小于等于12位");
+    if (!/^\w+$/.test(value)) errmsg.push("密码必须是英文、数字或下划线组成");
+    if (errmsg.length > 0) return Promise.reject(errmsg);
+    else return Promise.resolve();
   };
   render() {
     return (
@@ -22,17 +33,35 @@ export default class Login extends Component {
         {/* login 下部分 */}
         <div className="login-content">
           <h2>登录</h2>
-          <Form 
-          className="login-form" 
-          onFinish={this.onFinish}>
-            <Item name='username'>
+          <Form className="login-form" onFinish={this.onFinish}>
+            <Item
+              name="username"
+              rules={[
+                { required: true, message: "账号必须输入" }, //必填 用户声明式校验
+                { min: 4, message: "账号必须大于等于4位" },
+                { max: 12, message: "账号必须小于等于12位" },
+                {
+                  pattern: /^\w+$/,
+                  message: "账号必须是英文、数字或下划线组成"
+                }
+              ]}
+            >
               <Input
                 prefix={<UserOutlined className="site-form-item-icon" />}
                 placeholder="账号"
+                autoComplete="off"
               />
             </Item>
 
-            <Item name='password'>
+            <Item
+              name="password"
+              rules={[
+                // {validator:(_,value)=>
+                //   value? Promise.resolve():Promise.reject('请输入密码')
+                // }
+                { validator: this.pwdValidator }
+              ]}
+            >
               <Input
                 prefix={<LockOutlined className="site-form-item-icon" />}
                 type="password"
