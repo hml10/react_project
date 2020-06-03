72fce074bfd107df6a83538a93bcc8f70f86820a
hml <2665076656@qq.com>
2020-06-03 10:42:02 +0800

    项目完结 day_8

--- src/containers/admin/charts/line/line.jsx
+++ src/containers/admin/charts/line/line.jsx
@@ -1,7 +1,52 @@
 import React, { Component } from "react";
+import ReactEcharts from "echarts-for-react";
 
 export default class Line extends Component {
+  // 专门用于提供一个配置项
+  getOption = () => {
+    return {
+      //图标的标题
+      title: {
+        text: "服装销售", //标题的文字
+        textStyle: {
+          color: "red", //标题的样式
+        },
+      },
+      tooltip: {}, //鼠标滑动提示
+      //工具箱
+      toolbox: {
+        feature: {
+          saveAsImage: {}, //保存为图片
+        },
+      },
+      legend: {
+        //图例
+        data: ["销量", "库存"],
+      },
+      // x轴的配置
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      // y轴的配置
+      yAxis: {},
+      // 图标类型+数据
+      series: [
+        {
+          name: "销量",
+          // type: "bar",
+          type: "line",
+          data: [15, 20, 36, 10, 10, 20],
+        },
+        {
+          name: "库存",
+          type: "line",
+          data: [5, 10, 16, 22, 16, 36],
+        },
+      ],
+    };
+  };
+
   render() {
-    return <div>Line</div>;
+    return <ReactEcharts option={this.getOption()} />;
   }
 }
