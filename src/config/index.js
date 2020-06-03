579b11bed0e21bcd1c08e99323f88b462a78ec3d
hml <2665076656@qq.com>
2020-06-03 13:21:52 +0800

    项目完结 day_8 + 部署

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ export const WEATHER_LOCATION = "深圳"; //天气接口的授权码
 export const WEATHER_BASE_URL =
   "http://api.map.baidu.com/telematics/v3/weather"; // 天气请求的基本路径
 
-export const AJAX_BASE_URL = "http://localhost:3000"; //Ajax请求的基本路径
+export const AJAX_BASE_URL = ""; //Ajax请求的基本路径
 export const AJAX_TIMEOUT = "2000"; //Ajax请求的超时时间
 
 export const PAGE_SIZE = 4; //每一页展示多少条数据
